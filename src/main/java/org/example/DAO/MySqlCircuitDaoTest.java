584f3ecb3d1e7be2ece6d7d5557afeaefcfe96a5
DarrenMeidl <dmeidl15@gmail.com>
2024-04-06 14:43:21 +0100

    JUnit Tests 4-5: Created 1 more JUnit test for each feature

--- src/main/java/org/example/DAO/MySqlCircuitDaoTest.java
+++ src/main/java/org/example/DAO/MySqlCircuitDaoTest.java
@@ -15,24 +15,49 @@ class MySqlCircuitDaoTest { // by Darren Meidl
     }
     // INSERT CIRCUIT TESTS
     @Test
-    void insertCircuit() throws DaoException { // By Darren Meidl
+    void insertCircuitTestByName() throws DaoException { // By Darren Meidl
         System.out.println("TEST 1: 'insertCircuit()' ");
-        String expected = "testName"; // the expected name
-        Circuit test = new Circuit("testName", "testCountry", 1, 1); // create a test circuit
+        String expected = "t1"; // the expected name
+        Circuit test = new Circuit("t1", "t1Country", 1, 1); // create a test circuit
         int testID = dao.insertCircuit(test).getId(); // insert the test circuit & get its auto id from the database
         String actual = dao.getCircuitById(testID).getCircuitName(); // Get the name from the test Circuit from the test database
         assertEquals(expected, actual); // compare the strings
         System.out.println("EXPECTED: "+expected+" ACTUAL: "+actual); // print the results
+        System.out.println("");
     }
+    @Test
+    void insertCircuitTestByTurns() throws DaoException { // By Darren Meidl
+        System.out.println("TEST 2: 'insertCircuit()' ");
+        int expected = 2; // the expected number of turns
+        Circuit test = new Circuit("t2", "t2Country", 2, 2); // create a test circuit
+        int testID = dao.insertCircuit(test).getId(); // insert the test circuit & get its auto id from the database
+        int actual = dao.getCircuitById(testID).getTurns(); // Get the turns from the test Circuit from the test database
+        assertEquals(expected, actual); // compare the integers
+        System.out.println("EXPECTED: "+expected+" ACTUAL: "+actual); // print the results
+        System.out.println("");
+    }
+
     // UPDATE CIRCUIT TESTS
     @Test
-    void updateCircuit() throws DaoException { // By Darren Meidl
+    void updateCircuitTestByName() throws DaoException { // By Darren Meidl
         System.out.println("TEST 1: 'updateCircuit()' ");
-        String expected = "t2"; // the expected name
-        Circuit test = new Circuit("t2", "t2Country", 999, 2); // create a test circuit
+        String expected = "t1"; // the expected name
+        Circuit test = new Circuit("t1", "t1Country", 1, 1); // create a test circuit
         dao.updateCircuit(1, test); // update circuit with id 1 with the test circuit fields
         String actual = dao.getCircuitById(1).getCircuitName(); // Get the name from the test Circuit from the test database
         assertEquals(expected, actual); // compare the strings
+        System.out.println("EXPECTED: "+expected+" ACTUAL: "+actual); // print the results
+        System.out.println("");
+    }
+
+    @Test
+    void updateCircuitTestByTurns() throws DaoException { // By Darren Meidl
+        System.out.println("TEST 2: 'updateCircuit()' ");
+        int expected = 2; // the expected number of turns
+        Circuit test = new Circuit("t2", "t2Country", 2, 2); // create a test circuit
+        dao.updateCircuit(1, test); // update circuit with id 1 with the test circuit fields
+        int actual = dao.getCircuitById(1).getTurns(); // Get the turns from the test Circuit from the test database
+        assertEquals(expected, actual); // compare the integers
         System.out.println("EXPECTED: "+expected+" ACTUAL: "+actual); // print the results
         System.out.println("");
     }
