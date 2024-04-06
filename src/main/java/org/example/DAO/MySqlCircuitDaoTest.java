63a21fc1eee7d495b5bdbbb10430a2349e47e4bb
DarrenMeidl <dmeidl15@gmail.com>
2024-04-06 14:29:42 +0100

    JUnit Test 5: Test passed & runs correctly

--- src/main/java/org/example/DAO/MySqlCircuitDaoTest.java
+++ src/main/java/org/example/DAO/MySqlCircuitDaoTest.java
@@ -17,17 +17,23 @@ class MySqlCircuitDaoTest { // by Darren Meidl
     @Test
     void insertCircuit() throws DaoException { // By Darren Meidl
         System.out.println("TEST 1: 'insertCircuit()' ");
-        String expected = "testName";
-        Circuit test = new Circuit("testName", "testCountry", 1, 1);
+        String expected = "testName"; // the expected name
+        Circuit test = new Circuit("testName", "testCountry", 1, 1); // create a test circuit
         int testID = dao.insertCircuit(test).getId(); // insert the test circuit & get its auto id from the database
         String actual = dao.getCircuitById(testID).getCircuitName(); // Get the name from the test Circuit from the test database
-        assertEquals(expected, actual); // compare them
-        System.out.println("TEST 1: EXPECTED: "+expected+" ACTUAL: "+actual); // print the results
-        System.out.println("");
+        assertEquals(expected, actual); // compare the strings
+        System.out.println("EXPECTED: "+expected+" ACTUAL: "+actual); // print the results
     }
     // UPDATE CIRCUIT TESTS
     @Test
-    void updateCircuit() { // By Darren Meidl
-
+    void updateCircuit() throws DaoException { // By Darren Meidl
+        System.out.println("TEST 1: 'updateCircuit()' ");
+        String expected = "t2"; // the expected name
+        Circuit test = new Circuit("t2", "t2Country", 999, 2); // create a test circuit
+        dao.updateCircuit(1, test); // update circuit with id 1 with the test circuit fields
+        String actual = dao.getCircuitById(1).getCircuitName(); // Get the name from the test Circuit from the test database
+        assertEquals(expected, actual); // compare the strings
+        System.out.println("EXPECTED: "+expected+" ACTUAL: "+actual); // print the results
+        System.out.println("");
     }
 }
