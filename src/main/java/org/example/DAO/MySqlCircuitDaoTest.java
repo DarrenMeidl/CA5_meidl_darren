53cc2712e1faa4606ef175e608d62de3397dd5cb
DarrenMeidl <dmeidl15@gmail.com>
2024-04-06 13:59:17 +0100

    JUnit Tests 4-5: Coded first test

--- src/main/java/org/example/DAO/MySqlCircuitDaoTest.java
+++ src/main/java/org/example/DAO/MySqlCircuitDaoTest.java
@@ -1,24 +1,33 @@
 package org.example.DAO;
 
+import org.example.DTO.Circuit;
+import org.example.Exceptions.DaoException;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-class MySqlCircuitDaoTest {
+class MySqlCircuitDaoTest { // by Darren Meidl
     private MySqlCircuitDao dao; // declare
     @BeforeEach
     void setUp() {
         dao = new MySqlCircuitDao(); // initialize
     }
-
+    // INSERT CIRCUIT TESTS
     @Test
-    void insertCircuit() {
-
+    void insertCircuit() throws DaoException { // By Darren Meidl
+        System.out.println("TESTING: 'insertCircuit()' ");
+        String expected = "testName";
+        Circuit test = new Circuit(20, "testName", "testCountry", 1, 1);
+        dao.insertCircuit(test); // insert the test Circuit into the test database
+        String actual = dao.getCircuitById(20).getCircuitName(); // Get the name from the test Circuit from the test database itself
+        assertEquals(expected, actual);
+        System.out.println("EXPECTED: "+expected+" ACTUAL: "+actual);
+        System.out.println("");
     }
-
+    // UPDATE CIRCUIT TESTS
     @Test
-    void updateCircuit() {
+    void updateCircuit() { // By Darren Meidl
 
     }
 }
--- src/main/java/org/example/DAO/MySqlDao.java
+++ src/main/java/org/example/DAO/MySqlDao.java
@@ -10,7 +10,7 @@ public class MySqlDao
     public Connection getConnection() throws DaoException
     {
         String driver = "com.mysql.cj.jdbc.Driver";
-        String url = "jdbc:mysql://localhost:3306/F1";
+        String url = "jdbc:mysql://localhost:3306/F1_Test";
         String username = "root";
         String password = "";
         Connection connection = null;
