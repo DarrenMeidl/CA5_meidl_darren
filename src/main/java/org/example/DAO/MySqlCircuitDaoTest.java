5e95ae046bc2b59a9d6ab255f02c48332c0ec145
DarrenMeidl <dmeidl15@gmail.com>
2024-04-06 13:18:12 +0100

    JUnit Tests 4-5: Created Test class, dao object declared & initialized

---
+++ src/main/java/org/example/DAO/MySqlCircuitDaoTest.java
@@ -0,0 +1,24 @@
+package org.example.DAO;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MySqlCircuitDaoTest {
+    private MySqlCircuitDao dao; // declare
+    @BeforeEach
+    void setUp() {
+        dao = new MySqlCircuitDao(); // initialize
+    }
+
+    @Test
+    void insertCircuit() {
+
+    }
+
+    @Test
+    void updateCircuit() {
+
+    }
+}
