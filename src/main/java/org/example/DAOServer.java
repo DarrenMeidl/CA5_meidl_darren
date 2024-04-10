22026089484a55b4e1aed9667d2bd596fb5febe7
Darren <dmeidl15@gmail.com>
2024-04-10 22:47:48 +0100

    Created DAOServer class with imports

---
+++ src/main/java/org/example/DAOServer.java
@@ -0,0 +1,20 @@
+package org.example;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+public class DAOServer {
+    final int PORT_NUMBER = 11111;
+
+    public static void main(String[] args) {
+        DAOServer server = new DAOServer();
+        server.start();
+    }
+
+    public void start() {
+
+    }
+}
